3e2784eb64d4dcdfd9ee084ea558e467d4ceebfc
Hamza Afzal <hj786115@gmail.com>
2026-05-31 19:16:30 +0500

    iptv service

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://smartiptv-hj786115-9836s-projects.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
